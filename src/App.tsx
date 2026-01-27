e702e6a559c0acf7a047791bcbbc344c28ef458d
unknown <jangjaeko45@gmail.com>
2026-01-26 22:31:28 -0800

    feat : Ui library test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import "./App.css";
+import {
+  Button,
+  Label,
+  ListBox,
+  ListBoxItem,
+  Popover,
+  Select,
+  SelectValue,
+} from "react-aria-components";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <div></div>
+      <Select className="select">
+        <Label className="label">Favorite Animal</Label>
+        <Button className="trigger">
+          <SelectValue />
+        </Button>
+
+        <Popover className="popover">
+          <ListBox className="listbox">
+            <ListBoxItem className="item">Cat</ListBoxItem>
+            <ListBoxItem className="item">Dog</ListBoxItem>
+            <ListBoxItem className="item">Kangaroo</ListBoxItem>
+          </ListBox>
+        </Popover>
+      </Select>
     </>
   );
 }
